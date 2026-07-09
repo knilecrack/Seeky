f51948311d821eea711daa7ca4e0f778a19d87b9
mp3ric <mcmausknile@hotmail.com>
2026-07-10 00:05:48 +0200

    fixed pipline and fixed esbuild garbage

--- src/searchProvider.ts
+++ src/searchProvider.ts
@@ -71,10 +71,7 @@ async function getOrCreateFinder(basePath: string, storagePath?: string): Promis
 
     currentBasePath = normalizedBasePath;
     finderPromise = (async () => {
-        // Create an absolute URL to the ESM module relative to this file's CommonJS __dirname (dist)
-        const fffNodePath = require('url').pathToFileURL(require('path').join(__dirname, '..', 'node_modules', '@ff-labs', 'fff-node', 'dist', 'src', 'index.js')).href;
-        const mod = await new Function('url', 'return import(url)')(fffNodePath);
-        const FF = mod.FileFinder;
+        const { FileFinder: FF } = await import('@ff-labs/fff-node');
 
         let frecencyDbPath: string | undefined;
         let historyDbPath: string | undefined;
